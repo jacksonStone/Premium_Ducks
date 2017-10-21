53fce17f1e581e2319ed3ebc6acd7611ee0fe05a
Jackson Stone <jackson@skuid.com>
2017-10-21 15:42:01 -0400

    Main page beginnings

--- frontend/js/onload.js
+++ frontend/js/onload.js
@@ -1,4 +1,10 @@
 (()=>{
-	const hash = location.hash.substring(1);
-	pd.navigation.updatePage(hash);
+	function renderHash() {
+		const hash = location.hash.substring(1);
+		pd.navigation.updatePage(hash);
+	}
+	window.onhashchange = function() {
+ 		renderHash();
+	};
+	renderHash();
 })();
