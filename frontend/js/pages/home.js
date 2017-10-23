f4f0eeaade0aa7e533d21afb369c0476dfce690a
Jackson Stone <jackson@skuid.com>
2017-10-23 08:13:17 -0400

    Slight change to bottm preview

--- frontend/js/pages/home.js
+++ frontend/js/pages/home.js
@@ -1,12 +1,31 @@
-(()=>{
-	const favoriteDucks = [
-		'bishop',
-		'jack',
-		'bavarian'
-	];
+((pd) => {
+  function placeDucks (ducks) {
+  	const numOfFavorites = 6;
+    const favoritesContainer = document.getElementById('favorites-container');
+		let res = '<div class="single-favorite-container">';
+		let duckContainer = document.createElement('div');
+    for(let i = 0; i < numOfFavorites/3; i++) {
+    	res += '<div class="row">';
+    	for(let j = i*3; j < (i*3)+3; j++) {
+    		let duck = ducks[j];
+    		res += `
+    			<div class="col col-4" >
+    					<div class="favorite-canvas shadow">
+    						<img src="images/${duck.image}">
+    					</div>
+    					<h6>${duck.title}</h6>
+	    		</div>
+    		`;
+    	}
+    	res += '</div>';
+    }
+    res+='</div>';
 
-	pd.utils.requestDucks()
-		.then(ducks => {
-			console.log(ducks);
-		});
-})();
+    	duckContainer.innerHTML = res;
+    	favoritesContainer.appendChild(duckContainer);
+    
+  }
+
+  pd.utils.requestDucks()
+    .then(placeDucks)
+})(window.pd)
